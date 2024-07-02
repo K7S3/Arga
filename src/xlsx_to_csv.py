4a1f551976261dfe5e1d05c845d090ea99481664
K7S3 <ks2359@cornell.edu>
2024-07-02 18:10:36 -0400

    Initial Repo Setup

---
+++ src/xlsx_to_csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def convert_xlsx_to_csv(xlsx_file_path: str, csv_file_path: str) -> None:
+    """
+    Converts an Excel file to a CSV file.
+
+    Args:
+        xlsx_file_path (str): The path to the Excel file.
+        csv_file_path (str): The path to the CSV
+    
+
+    Returns
+        None
+    """
+
+    # Read the Excel file
+    data_xls = pd.read_excel(xlsx_file_path, index_col=None)
+
+    # Convert the Excel file to a CSV file
+    data_xls.to_csv(csv_file_path, encoding='utf-8', index=False)
+
+convert_xlsx_to_csv('../data/raw/AI Test-1.xlsx', '../data/raw/AI Test-1.csv')
